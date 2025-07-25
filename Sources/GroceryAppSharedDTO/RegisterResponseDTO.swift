f8a669ae92d2951f102e087f3a0752c2377a7a94
sunnyegbo <sunnyegbo@yahoo.com>
2025-07-25 14:07:52 -0700

    register and login response

---
+++ Sources/GroceryAppSharedDTO/RegisterResponseDTO.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterResponseDTO.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Sunny Egbo on 7/25/25.
+//
+
+import Foundation
+
+public struct RegisterResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+
+    public init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+}
