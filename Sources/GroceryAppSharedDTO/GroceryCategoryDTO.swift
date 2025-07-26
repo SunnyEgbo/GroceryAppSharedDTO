e65e7f343a01589336363bde149fac6d7e79cbf6
sunnyegbo <sunnyegbo@yahoo.com>
2025-07-25 18:48:26 -0700

    Adds both the request and the response DTOs for Grocery Category

--- Sources/GroceryAppSharedDTO/GroceryCategoryDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryDTO.swift
@@ -1,8 +1,30 @@
 //
-//  File.swift
+//  GroceryCategoryDTO.swift
 //  GroceryAppSharedDTO
 //
 //  Created by Sunny Egbo on 7/25/25.
 //
 
 import Foundation
+
+public struct GroceryCategoryRequestDTO: Codable {
+    public var title: String
+    public var colorCode: String
+    
+    public init(title: String, colorCode: String) {
+        self.title = title
+        self.colorCode = colorCode
+    }
+}
+
+public struct GroceryCategoryResponseDTO: Codable {
+    public var id: UUID
+    public var title: String
+    public var colorCode: String
+    
+    public init(id: UUID, title: String, colorCode: String) {
+        self.id = id
+        self.title = title
+        self.colorCode = colorCode
+    }
+}
