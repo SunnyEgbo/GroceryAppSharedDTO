f399e3e09796b0e9f3da80c6e004633a716752fb
sunnyegbo <sunnyegbo@yahoo.com>
2025-07-27 09:23:34 -0700

    Adds the the real request and response DTOs for Grocery Items

--- Sources/GroceryAppSharedDTO/GroceryItemDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryItemDTO.swift
@@ -1,8 +1,34 @@
 //
-//  File.swift
+//  GroceryItemDTO.swift
 //  GroceryAppSharedDTO
 //
 //  Created by Sunny Egbo on 7/27/25.
 //
 
 import Foundation
+
+public struct GroceryItemRequestDTO: Codable {
+    public var title: String
+    public var price: Decimal
+    public var quantity: Int
+    
+    public init(title: String, price: Decimal, quantity: Int) {
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+}
+
+public struct GroceryItemResponseDTO: Codable {
+    public var id: UUID
+    public var title: String
+    public var price: Decimal
+    public var quantity: Int
+
+    public init(id: UUID, title: String, price: Decimal, quantity: Int) {
+        self.id = id
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+}
